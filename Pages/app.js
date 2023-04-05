ade1605217a5e976bee173455639edd305cd4cc0
Gayanuka Amarasuriya <gayanukaamarasuriya@gmail.com>
2023-04-05 23:45:54 +0530

    Save like count

--- Pages/app.js
+++ Pages/app.js
@@ -1,8 +1,13 @@
 var likeBtn = document.getElementById('like-btn');
 var likeCount = document.getElementById('like-count');
 
+if (localStorage.getItem("likeCount")) {
+    likeCount.innerHTML = localStorage.getItem("likeCount"); // Get the like count from local storage
+}
+
 likeBtn.addEventListener("click", function(){
     var count = parseInt(likeCount.innerHTML); // Get the current like count
     count++;
     likeCount.innerHTML = count;
+    localStorage.setItem("likeCount", count); // Store the like count in local storage
 });
