c131629644470668c167e82888a7ac9670f1a7d7
Gayanuka Amarasuriya <gayanukaamarasuriya@gmail.com>
2023-04-29 10:22:59 +0530

    Revert "Updated like count storage"
    This reverts commit f83c39a260a6bd02c0ab919f6534d296a9f84800.

--- Pages/app.js
+++ Pages/app.js
@@ -1,48 +1,16 @@
-const likeButton = document.getElementById('like-buttonn');
-const likeCount = document.getElementById('like-count');
+var likeBtn = document.getElementById('like-btn');
+var likeCount = document.getElementById('like-count');
 
-// if (localStorage.getItem("likeCount")) {
-//     likeCount.innerHTML = localStorage.getItem("likeCount"); // Get the like count from local storage
-// }
-
-// likeBtn.addEventListener("click", function(){
-//     var count = parseInt(likeCount.innerHTML); // Get the current like count
-//     count++;
-//     likeCount.innerHTML = count;
-//     localStorage.setItem("likeCount", count); // Store the like count in local storage
-// });
-
-let count = 0; // initialize the like count to 0
-
-// send an AJAX request to the server to update the like count
-function updateLikeCount() {
-  const xhr = new XMLHttpRequest();
-  xhr.open('POST', '/update-like-count', true);
-  xhr.setRequestHeader('Content-type', 'application/json');
-  xhr.onload = function() {
-    if (xhr.status === 200) {
-      count = JSON.parse(xhr.responseText).count; // update the like count with the response from the server
-      likeCount.textContent = `${count} likes`; // display the updated like count
-    }
-  };
-  xhr.send(JSON.stringify({ count }));
+if (localStorage.getItem("likeCount")) {
+    likeCount.innerHTML = localStorage.getItem("likeCount"); // Get the like count from local storage
 }
 
-likeButton.addEventListener('click', function() {
-  count++; // increment the like count
-  updateLikeCount(); // send the like count to the server to update it
+likeBtn.addEventListener("click", function(){
+    var count = parseInt(likeCount.innerHTML); // Get the current like count
+    count++;
+    likeCount.innerHTML = count;
+    localStorage.setItem("likeCount", count); // Store the like count in local storage
 });
-
-// display the initial like count when the page loads
-const xhr = new XMLHttpRequest();
-xhr.open('GET', '/get-like-count', true);
-xhr.onload = function() {
-  if (xhr.status === 200) {
-    count = JSON.parse(xhr.responseText).count; // get the like count from the server response
-    likeCount.textContent = `${count} likes`; // display the like count on the page
-  }
-};
-xhr.send();
 
 var loader = document.getElementById("preloader");
 
